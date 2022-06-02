f1be4d7d93f7870c1784ba18460af576ac57d81c
Ahmad Raza <razaahmad93@gmail.com>
2022-06-02 18:24:11 +0500

    main screen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:carify_app/controller/buyer_provider.dart';
 import 'package:carify_app/controller/seller_provider.dart';
 import 'package:carify_app/screens/sign_in_up_process/account_type/Account_screen.dart';
-import 'package:carify_app/screens/sign_in_up_process/name_info_buyer/name_screen_buyer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -37,7 +36,7 @@ void main() async {
        //  SignInUpScreen(),
       home:
       // AddPhoto()
-      NameScreenBuyer(),
+       AccountType(),
     ),
   ));
 }
